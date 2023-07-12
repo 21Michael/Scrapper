5439b02774e3db22a6fcd1481b26e68a2e6cb359
Mikhail <mykhailo.rudiev@aimprosoft.com>
2023-07-12 13:30:48 +0300

    add scrapper service

---
+++ services/scrapper.ts
@@ -0,0 +1,169 @@
+import { Browser } from 'puppeteer';
+import { EXP_LVL_ARRAY, COMPANY_TYPE_ARRAY, ENGlISH_LVL_ARRAY, MONTH_UKR_ARRAY, CITY_UKR_ARRAY } from '../constants';
+import { ICandidate, IVacancy } from '../types';
+
+export const scrapPagination = async ({
+    url,
+    browser,
+}:{
+    url: string;
+    browser: Browser;
+}) => {
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    const paginationPages = await page.evaluate(() => {
+        const paginationPages = [];
+
+        const paginationElement = document.querySelector('.pagination');
+        const pageElements = Array.from(paginationElement.querySelectorAll('.page-item > a'));
+        const lastPageElement = pageElements[pageElements.length - 2];
+
+        const lastPage = {
+            href: lastPageElement.getAttribute('href'),
+            num: Number(lastPageElement.innerHTML)
+        };
+
+        for (let i = 1; i <= lastPage.num; i++) {
+            paginationPages.push({
+                href: '&page=' + String(i),
+                num: i
+            });
+        }
+
+        return paginationPages;
+    });
+
+    return paginationPages;
+};
+
+
+export const scrapCandidates = async ({
+    url,
+    browser,
+}:{
+    url: string;
+    browser: Browser;
+}) => {
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    const candidatesPages: ICandidate[] = await page.evaluate((EXP_LVL_ARRAY, COMPANY_TYPE_ARRAY, ENGlISH_LVL_ARRAY, MONTH_UKR_ARRAY, CITY_UKR_ARRAY) => {
+        const englishLevelRegExp = new RegExp(`${ENGlISH_LVL_ARRAY.join('|')}+`, 'g');
+        const expLVLRegExp = new RegExp(`${EXP_LVL_ARRAY.join('|')}+`, 'g');
+        const dateRegExp = new RegExp(`(сьогодні)|(вчора)|(${MONTH_UKR_ARRAY.join('|')}+)`, 'g');
+        const cityRegExp = new RegExp(`${CITY_UKR_ARRAY.join('|')}+`, 'g');
+
+        const candidatesElement = document.querySelector('.searchresults');
+        const candidatesElements = Array.from(candidatesElement.querySelectorAll('.card'));
+
+        return candidatesElements.map(candidate => {
+            const developerDetailsElement = candidate.querySelector('.order-1').lastElementChild;
+            const developerDetailsElementText = developerDetailsElement?.textContent;
+            const nameText = candidate.querySelector('.profile').textContent;
+            const href = candidate.querySelector('.profile').getAttribute('href');
+            const salaryText = candidate.querySelector('.text-success').textContent;
+            const viewsText = candidate.querySelector('.card-footer').lastElementChild.textContent;
+            const skillsText = candidate.querySelector('.card-body').lastElementChild.lastElementChild.textContent;
+
+            const englishLevel = developerDetailsElementText.match(englishLevelRegExp)?.[0] || null;
+            const expLVL = developerDetailsElementText.match(expLVLRegExp)?.[0] || null;
+            const date = developerDetailsElementText.match(dateRegExp)?.[0] || null;
+            const city = developerDetailsElementText.match(cityRegExp)?.[0] || null;
+            const name = nameText.match(/\w+/g)?.join(' ') || null;
+            const salary = salaryText.match(/\$\d+/g)?.[0] || null;
+            const views = viewsText.match(/\d+/g)?.[0] || null;
+            const skills = skillsText.match(/\w+/gi) || null;
+
+            return {
+                name,
+                href,
+                salary,
+                date,
+                englishLevel,
+                expLVL,
+                city,
+                views,
+                skills
+            };
+        });
+
+    }, EXP_LVL_ARRAY, COMPANY_TYPE_ARRAY, ENGlISH_LVL_ARRAY, MONTH_UKR_ARRAY, CITY_UKR_ARRAY);
+
+    await page.close();
+
+    return candidatesPages;
+};
+
+export const scrapVacancies = async ({
+    url,
+    browser,
+}:{
+    url: string;
+    browser: Browser;
+}) => {
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    const vacanciesPages: IVacancy[] = await page.evaluate((EXP_LVL_ARRAY, COMPANY_TYPE_ARRAY, ENGlISH_LVL_ARRAY) => {
+        const englishLevelRegExp = new RegExp(`${ENGlISH_LVL_ARRAY.join('|')}+`, 'g');
+        const companyTypeRegExp = new RegExp(`${COMPANY_TYPE_ARRAY.join('|')}+`, 'g');
+        const expLVLRegExp = new RegExp(`${EXP_LVL_ARRAY.join('|')}+`, 'g');
+
+        const vacancyElement = document.querySelector('.list-jobs');
+        const vacancyElements = Array.from(vacancyElement.querySelectorAll('li'));
+
+        return vacancyElements.map(vacancy => {
+            const dateText = vacancy.querySelector('.text-date').firstChild.textContent;
+            const viewsText = vacancy.querySelectorAll('.text-date > span')[0].textContent;
+            const responsesText = vacancy.querySelectorAll('.text-date > span')[1].textContent;
+            const salaryText = vacancy.querySelector('.public-salary-item')?.textContent;
+            const locationText = vacancy.querySelector('.location-text')?.textContent;
+            const jobDetailsElementText = vacancy.querySelector('.list-jobs__details__info')?.textContent;
+            const titleElement = vacancy.querySelector('.list-jobs__title > a');
+            const titleElementText = titleElement.textContent;
+
+            const salaryFork = salaryText && {
+                min: salaryText.match(/\d+/g)[0],
+                max: salaryText.match(/\d+/g)[1],
+            };
+            const location = {
+                relocate: locationText.includes('Релокейт'),
+                countries: locationText.match(/(?![^(]*\))([^\(\)]+)/g)?.[0].replace('Релокейт','')?.match(/[а-яії]+/gi),
+                cities: locationText.match(/\((.*)?\)/g)?.[0].match(/[а-яії]+/gi),
+            };
+
+            const englishLevel = jobDetailsElementText.match(englishLevelRegExp)?.[0] || null;
+            const companyType = jobDetailsElementText.match(companyTypeRegExp)?.[0] || null;
+            const expLVL = jobDetailsElementText.match(expLVLRegExp)?.[0] || null;
+            const date = dateText.match(/(\d+\s[а-я]+\s*|сьогодні)/)?.[0].trim() || null;
+            const views = viewsText.match(/(\d+)/)?.[0].trim() || null;
+            const responses = responsesText.match(/(\d+)/)?.[0].trim() || null;
+            const name = titleElementText.match(/(\w+)/gi)?.join(' ') || null;
+            const href = titleElement.getAttribute('href') || null;
+
+            return {
+                name,
+                href,
+                date,
+                views,
+                responses,
+                salaryForkMin: salaryFork?.min || null,
+                salaryForkMax: salaryFork?.max || null,
+                relocate: location?.relocate,
+                countries: location?.countries || null,
+                cities: location?.cities || null,
+                englishLevel,
+                companyType,
+                expLVL
+            };
+        });
+    }, EXP_LVL_ARRAY, COMPANY_TYPE_ARRAY, ENGlISH_LVL_ARRAY);
+
+    await page.close();
+
+    return vacanciesPages;
+};
