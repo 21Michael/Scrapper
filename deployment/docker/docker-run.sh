f66ce864f0f0697411c8f0587b05f9d51f226839
Mikhail <mykhailo.rudiev@aimprosoft.com>
2023-07-24 15:46:44 +0300

    mode docker config

---
+++ deployment/docker/docker-run.sh
@@ -0,0 +1,11 @@
+# 1) build and run
+docker compose up -d --build
+
+## 2) Stop Remove containers
+#docker compose down
+
+## 3) Push images to docker hub
+#docker push mikhail21/scrapper-service:latest
+#docker push mikhail21/scrapper-api:latest
+#docker push mikhail21/scrapper-client-dev:latest
+#docker push mikhail21/scrapper-client-prod:latest
